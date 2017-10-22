fcfdcdfc0eb274111cfa757dc018128925499101
MXout <819028939@qq.com>
2017-10-22 21:02:26 +0800

    added the function to show weather

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Administrator on 2017/10/22 0022.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
